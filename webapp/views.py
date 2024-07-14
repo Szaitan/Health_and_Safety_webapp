99fd398edfdd8f6b108e908f7e345db8dda82ff3
Michal <m.krepiniewicz@gmail.com>
2024-07-14 11:28:36 +0200

    Warsaw-home - Testing request.user and creation of user from scratch - ver.02

--- webapp/views.py
+++ webapp/views.py
@@ -23,7 +23,7 @@ class IntroPageView(View):
         #     print(request.user.user_projects.all()[0].name)
 
         # Test with creation of superuser
-        # CustomUser.objects.create_user(username="test2", email="krepcio@wp.pl", password="A1@sd$45G$",
+        # CustomUser.objects.create_user(username="test2", email="", password="",
         #                                first_name="Dupa", last_name="Dupp", user_type="normal")
         return render(request, "webapp/intro_page.html", {
             "year": get_year()
